48917d50881451eb6957cd8cff021ec486415379
NguyenDuy <nguyenduy27092002@gmail.com>
2021-01-20 11:04:13 +0700

    I fix somthing Signed-off-by: NguyenDuy <nguyenduy27092002@gmail.com>

--- Project/Js/javascript.js
+++
@@ -1,6 +0,0 @@
-function openFormChat() {
-	document.getElementById("box_chat").style.display = "block";
-}
-function closeFormChat() {
-	document.getElementById("box_chat").style.display = "none";
-}
